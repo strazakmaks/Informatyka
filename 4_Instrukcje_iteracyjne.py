542ce2f9a81569e69fe28201b7a3f2abab21bf3b
Maksymilian Strazynski <strazynskimaksymilian@gmail.com>
2025-12-13 21:58:22 +0000

    poprawa czwartej lekcji, oraz dodanie selektora lekcji do main.py aby w replit można było uruchomić daną lekcję po kliknięciu ikonki play

--- 4_Instrukcje_iteracyjne.py
+++ 4_Instrukcje_iteracyjne.py
@@ -108,7 +108,7 @@ print()
   
 i= 10
 while i >= 2:
-  if i % 3 != 0:
+  if i % 3 == 0:
     i -=1 
     continue
   print(i, end=" ")
--- main.py
+++ main.py
@@ -1,2 +1,20 @@
-#print("lekcja 1")
-# ctrl/ komentarz
+import os
+
+print("Wybierz lekcję do uruchomienia:")
+print("1. Etapy rozwiązywania problemów")
+print("2. Podstawy Pythona")
+print("3. Instrukcje warunkowe")
+print("4. Instrukcje iteracyjne")
+
+wybor = input("Twój wybór (1-4): ")
+
+if wybor == "1":
+    os.system("python3 1_etapy_rozwiązywania_problemów_za_pomocą_komputera.py")
+elif wybor == "2":
+    os.system("python3 2_Podstawy_pythona.py")
+elif wybor == "3":
+    os.system("python3 3_Instrukcje_warunkowe_w_Pythonie.py")
+elif wybor == "4":
+    os.system("python3 4_Instrukcje_iteracyjne.py")
+else:
+    print("Nieprawidłowy wybór")
