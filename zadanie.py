369a5f659ebd31a35dc0c4c1881124e361bd7f4c
Maksymilian Strazynski <strazynskimaksymilian@gmail.com>
2026-01-25 17:53:18 +0000

    skończyłem zadanie

--- zadanie.py
+++ zadanie.py
@@ -2,23 +2,45 @@
 
 licznik1 = int(input("Podaj pierwszy licznik: "))
 mianownik1 = int(input("Podaj pierwszy mianownik: "))
+while True:
+    mianownik1 = int(input("Podaj pierwszy mianownik: "))
+    if mianownik1 == 0:
+        print("Mianownik nie może być zerem! Spróbuj ponownie.")
+    else:
+        break
+
 licznik2 = int(input("Podaj drugi licznik: "))
 mianownik2 = int(input("Podaj drugi mianownik: "))
+while True:
+    mianownik2 = int(input("Podaj pierwszy mianownik: "))
+    if mianownik2 == 0:
+        print("Mianownik nie może być zerem! Spróbuj ponownie.")
+    else:
+        break
 
 def NWD (a,b):
   while b>0:
     a,b=b,a%b
   return a
-  
+
 def NWW(a,b):
   return (a*b)//NWD(a,b)
-  
+
 NWW1 = (NWW(mianownik2,mianownik1))
 
 licznik11 = licznik1*(NWW1//mianownik1)
 licznik22 = licznik2*(NWW1//mianownik2)
-
 licznik3 = licznik11 + licznik22
+
+NWD1 = (NWD(licznik3,NWW1))
+licznik3 = licznik3//NWD1
+NWW1 = NWW1//NWD1
+
 liczbaCalkowita = licznik3//NWW1
 licznik4 = licznik3%NWW1
-print(f"{liczbaCalkowita} {licznik4}/{NWW1}")
+if licznik4 == 0:
+  print(f"{liczbaCalkowita}")
+elif liczbaCalkowita == 0:
+  print(f"{licznik4}/{NWW1}")
+else:
+  print(f"{liczbaCalkowita} {licznik4}/{NWW1}")
